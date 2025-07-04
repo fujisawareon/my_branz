194c72dee1c9f9060cf9e09b0b827dde981c8c46
fujisawareon <r-fujisawa@sara.co.jp>
2025-07-04 15:03:37 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        react(),
+    ],
+    optimizeDeps: {
+        include: ['moment', 'jquery', 'daterangepicker'],
+    },
+    server: {
+        host: true,
+        cors: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
+});
